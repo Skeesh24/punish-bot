b56eb90e0269d7d2d937e925ef098d2fb8539702
pakulov_de <pakulov_de@devtrix.ru>
2024-11-13 04:26:22 +0500

    minor fixes

--- src/main.py
+++ src/main.py
@@ -1,16 +1,22 @@
 import asyncio
+import logging
 
 from dotenv import load_dotenv
 
 from handlers import bot, dp
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 load_dotenv()
 
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
+    logger.info("Connecting to Telegram API...")
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
+    logger.info("Bot is starting...")
     asyncio.run(main())
